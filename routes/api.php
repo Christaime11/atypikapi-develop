5c56d0197196f76a05f8b5e121b63452a0986d07
Christaime <linusague@gmail.com>
2021-03-20 08:20:41 +0100

    JWT

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,11 @@ use App\Http\Controllers\Api\CommentairesController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+|*/
+
 Route::middleware('cors')->group(function(){
-    Route::post('/register', [AuthController::class,'register']); //@Todo secure with client credentials
     Route::post('/login', [AuthController::class,'login']);
+    Route::post('/register', [AuthController::class,'register']); //@Todo secure with client credentials
     Route::get('email/verify/{id}', [VerificationController::class,'verify'])->name('verification.verify');
     Route::get('email/resend', [VerificationController::class,'resend'])->name('verification.resend');
 
@@ -33,6 +33,7 @@ Route::middleware('cors')->group(function(){
 
     });
 });
+
 
 Route::middleware(['auth:api','cors'])->group( function(){
     Route::prefix('habitats')->group( function(){
